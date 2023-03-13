96ac860186061acfbc5fad982f4fb8eecbb7c5e0
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-03-13 19:15:59 -0400

    feat: add login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { Button, Container } from "../styles/styles";
+import * as S from "../components/Form/Form.style";
+import * as SharedStyle from "../styles/styles";
+import LoginForm from "../components/Form/LoginForm";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const LoginScreen = ({ navigation }: any) => {
   return (
-    <Container>
-      <Button
-        onPress={() => {
-          navigation.push("Splash");
-        }}
-      >
-        Go back
-      </Button>
-    </Container>
+    <S.ContainerLogin>
+      <SharedStyle.StatusBar />
+      <S.WrapperTitleLogin>
+        <S.TitleTextLogin>Welcome back</S.TitleTextLogin>
+      </S.WrapperTitleLogin>
+
+      <S.WrapperForm>
+        <S.ScrollView>
+          <LoginForm />
+        </S.ScrollView>
+      </S.WrapperForm>
+    </S.ContainerLogin>
   );
 };
 
